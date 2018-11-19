d590c1257b7408cbbb260609df1b910e4ac200b8
Rahman85 <45152632+Rahman85@users.noreply.github.com>
2018-11-18 22:54:35 -0500

    added some functions

--- sandbox/src/main/java/ru/stqa/pro/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pro/sandbox/MyFirstProgram.java
@@ -2,10 +2,32 @@ package ru.stqa.pro.sandbox;
 
 public class MyFirstProgram { //class name starts with uppercase
 	//function
-	public static void main(String[] args) { // starts with lower case
-		System.out.println("Hello, world"); //executed code
-		System.out.println();
+	public static void main(String[] args) {// starts with lower case: void nothing return
+		hello("world");
+		hello("user");
+		hello("rahman");
+		double len = 5;
+		System.out.println("Square of square with side " + len + " = " + area(len));
 
-       }
+		double a = 4;
+		double b = 6;
+		System.out.println("Square of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+
+	}
+
+  public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody);
+
+	}
+
+	public static double area(double l) {
+		return l * l;
+
+	}
+	public static double area(double a, double b) {
+		return a * b;
+
+	}
+
 }
 	
